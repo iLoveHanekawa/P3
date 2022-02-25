d375410839dd8f61bda638e33c6e3a28b2587e7d
Arjun Tanwar <arjunthakur900@gmail.com>
2022-02-25 20:10:11 +0530

    deploy

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const inputBtn = document.getElementById("input-btn")
-const myLead = ["apple", "banana", "cherry"]
+const myLead = []
 const inputEl = document.getElementById("input-el")
 inputBtn.addEventListener("click", function (){
     myLead.push(inputEl.value) 
+    renderLeads()
+    inputEl.value = ""
 })
 const ulEl = document.getElementById("ul-el")
-for(let i = 0; i < myLead.length; i++) {
-    ulEl.innerHTML += "<li>" + myLead[i] + "</li>"
+function renderLeads() {    
+    let listItems = ""
+    for(let i = 0; i < myLead.length; i++) {
+        listItems += `
+            <li>
+                <a href = '${myLead[i]}' target = '_blank'>${myLead[i]}</a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
